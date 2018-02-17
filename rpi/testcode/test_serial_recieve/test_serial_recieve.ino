f25fa6b5c07c85e432f8672eb73855949ea56603
kirstyc <kirstycha@gmail.com>
2018-02-17 16:55:59 -0700

    files to test serial writing pi -> arduino

---
+++ rpi/testcode/test_serial_recieve/test_serial_recieve.ino
@@ -0,0 +1,8 @@
+//arduino file to test serial recieving 
+
+void setup () {
+Serial.begin(9600);
+}
+
+void loop() {
+}
---
+++ rpi/testcode/testserial.js
@@ -0,0 +1,38 @@
+//test code for serial input 
+
+var SerialPort = require('serialport');
+var port = new SerialPort('/dev/ttyACMO',{ baudRate: 9600});
+
+port.write('main screen turn on', function(err) {
+	if (err) {
+		return console.log('Error on write: ', err.message);
+		
+	}
+	console.log('message written');
+}
+};
+
+//open errors will be emitted as an error event
+port.on('error', function(err) {
+	console.log('Error: ', err.message);
+}
+);
+
+// Switches the port into "flowing mode"
+port.on('data', function (data) {
+  console.log('Data:', data);
+});
+
+// Read data that is available but keep the stream from entering "flowing mode"
+port.on('readable', function () {
+  console.log('Data:', port.read());
+});
+
+port.write('testing', function(err) {
+	if (err) {
+		return console.log('Error on write: ', err.message);
+		
+	}
+	console.log('test written');
+}
+};
